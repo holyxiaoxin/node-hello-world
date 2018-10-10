4525bd14806fff06ef5b54a3ae47e3ed3f02d30d
Jia Rong <jr.loves.maths@gmail.com>
2018-10-11 00:18:46 +0800

    bump

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http');
 const host = '0.0.0.0'
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('1111\n');
+  res.end('2222\n');
 }).listen(3000, host);
 console.log(`Server running at http://${host}:3000/`);
